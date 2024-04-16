11ce2c66061897ac25e52a3cccb1105aa0fa8aea
B10923011 <b10923011@gemail.yuntech.edu.tw>
2024-04-16 12:15:01 +0800

    手機版更新

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 let s1_title = document.getElementsByClassName('s1-title');
 let s1_sm_title = document.getElementsByClassName('s1-sm-title');
+let footer = document.getElementsByClassName('footer');
+let logo = document.getElementsByClassName('logo');
 
 window.addEventListener('resize', function() {
     if (window.innerWidth < 768) {
         s1_title[0].style.display = 'none';
         s1_sm_title[0].style.display = 'block';
+        footer[0].style.display = 'none';
     } else {
         s1_title[0].style.display = 'block';
         s1_sm_title[0].style.display = 'none';
+        footer[0].style.display = 'block';
     }
 });
+
