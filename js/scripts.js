7310fa5113fcfa82a288ff297f5e296c9d4fe229
B10923011 <b10923011@gemail.yuntech.edu.tw>
2024-04-16 14:13:59 +0800

    手機版更新

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,19 @@ let s1_sm_title = document.getElementsByClassName('s1-sm-title');
 let footer = document.getElementsByClassName('footer');
 let logo = document.getElementsByClassName('logo');
 
+// 初始化元素的顯示狀態
+if (localStorage.getItem('windowWidth') < 768) {
+    s1_title[0].style.display = 'none';
+    s1_sm_title[0].style.display = 'block';
+    footer[0].style.display = 'none';
+} else {
+    s1_title[0].style.display = 'block';
+    s1_sm_title[0].style.display = 'none';
+    footer[0].style.display = 'block';
+}
+
 window.addEventListener('resize', function() {
+    localStorage.setItem('windowWidth', window.innerWidth);
     if (window.innerWidth < 768) {
         s1_title[0].style.display = 'none';
         s1_sm_title[0].style.display = 'block';
@@ -14,4 +26,3 @@ window.addEventListener('resize', function() {
         footer[0].style.display = 'block';
     }
 });
-
